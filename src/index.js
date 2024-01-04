aff8edf68f05b9a80cd98edb276d425a0099b18f
c-robinson23 <147357577+c-robinson23@users.noreply.github.com>
2024-01-04 12:10:32 -0500

    Added the search functionality using API
    Adding API with Axios using AJAX method
    
    1. Get axios link form GitHub and add to index.html in <head>
    	to see if working correctly: inspect webpage - console - axios (should return: f (){return e.apply(t,arguments)}
    
    2. Get API key (set as a variable)
    Create a new function called search and this function is going to receive a city name and then based on that it will update the temp (the api key will be given depending on api source URL)
    	function searchCity(city) {}
    
    3. Get API URL (set as a variable) to make sure the URL works…go to webpage and enter a city and the console should show a website link that gives the weather information. (Console.log(apiUrl);) gives the temperature of city entered in the console.
    
    4. Now to get the current temperature:
    axios.get(apiUrl).then(refreshWeather);
    	function refreshWeather(response)
    	this is the function that receives the response JSON object returned from the axios-api search
            console.log(response.data) to get the city information
    
    5. Now replace the default temperature with the actual current temperature of the city.
    Add an ID to the class=“weather-app-temperature” and name it “temperature”.
    	Math.round(declared variable) to get only the whole number.
    
    6. Now fix the user input for city format if it it mistyped or something. Developer wants to use the API result name to get the correct city information so the the user input needs to match.
            a. remove **this from city Element from function handleSearchSubmit(event){} to function refreshWeather(response){} under let temperature = response.data.temperature.current;
    		**this: let cityElement = document.querySelector("#city");
      cityElement.innerHTML = searchInput.value;
    	b. Change cityElement.innerHTML = searchInput.value; to cityElement.innerHTML = response.data.city;
    
    7. Set the landing page (at the end of the .js code)
    Remove the default city and temperature in the .html code (Paris, 72) to leave blank.
    Add a city for when the landing page is refreshed without user input of a city (example. Brooklyn)
    	searchCity(“Brooklyn”);
    -the axios-api will automatically display the default current temp of city Brooklyn
    	a. Call SearchCity when we reload the page
    	b. Then it will go inside the function searchCity(city){}
    	c. Makes a call to the api
    	d. Then replace the two elements to display city and temp

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
+function refreshWeather(response) {
+  //console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  // make api call and update the interference
+  let apiKey = "8955aec3fa84eee934db0tdod70f6d28";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   //console.log(searchInput.value);
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Brooklyn");
