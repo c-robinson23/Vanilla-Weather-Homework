f146f8ac5e2ab7972fe60888689fd3f9cfda69e9
c-robinson23 <147357577+c-robinson23@users.noreply.github.com>
2024-01-04 15:43:19 -0500

    Injected more weather data
    Included the missing data on the application: Date Time Condition Humidity Wind
    
    For Condition:
    
    1. Use <span></span>
    
    2. Delete the .html default description: “scatted clouds”, put span here to stay inline.
    
    3. Create a span id for description of weather: <span id=“description”></span>
        Console.log(response.data.condition.description)
    
    4. Refresh the page and under function refreshWeather(response){} - console.log(response.data) to get the city information from the api (turn into a comment afterwards to not interfere with the code)
    
    5. Create a variable for the descriptionElement and inject into the innerHTML code
    let descriptionElement = document.querySelector("#description");
    descriptionElement.innerHTML = response.data.condition.description;
    
    For Humidity & Wind-Speed:
    In .html file code
    <span id=“humidity”></span>  and <span id=“wind-speed”></span> within the <strong></strong>
    
    In .js file code
    Create variables under the function refreshWeather(response){}
    let humidityElement = document.querySelector("#humidity");
      let windSpeedElement = document.querySelector("#wind-speed");
    
    Now interject into the .innerHTML
    
    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
      windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
    
    For Day and Time:
    
    In .html file
    Create an id using <span id=“time”></span>
    
    In .js file code
    let timeElement = document.querySelector("#time");
    timeElement.innerHTML = "Tuesday 14:49";
    
    Now to get the time in the correct format, developer needs to parse the time given from the api using console.log(response.data.time * 1000) ——this will give the time in the correct format and year
    
    Then create the timeElement.innerHTML = date.getHours() to get the first two digits of the time for example
    
    For the day, the console.log(date.getDate()) it will return a number for the day. Example: 2=Tuesday, 4=Thursday)
    
    Create a new function to get the correct day format. Returns a formatted date from API response.
    function formatDate(date){}
     *****make sure to include inside the function:     if (minutes <10) { minutes = `0${minutes}`;}
    ******this inputs a 0 if the minutes in the time is less than 10.
    
    timeElement.innerHTML = `${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
    Becomes
    timeElement.innerHTML = formatDate(date);
    
    Using the created function for the date format to replace the parse from before

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
 function refreshWeather(response) {
   //console.log(response.data);
+  //console.log(response.data.condition.description);
+  //console.log(response.data.temperature.humidity);
+  //console.log(response.data.wind.speed);
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.city;
+  let timeElement = document.querySelector("#time");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let date = new Date(response.data.time * 1000);
 
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
